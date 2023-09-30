05f5d2d99fa70aacb72c8237ece2301b66edde12
Anthony Venezia <wavenezia@gmail.com>
2023-09-30 16:26:32 -0400

    Update 2DRipple.js 400 to 75

--- 2DRipple.js
+++ 2DRipple.js
@@ -10,7 +10,7 @@ let dampening = 0.99;
 
 function setup() {
   pixelDensity(1);
-  createCanvas(600, 400);
+  createCanvas(600, 75);
   cols = width;
   rows = height;
   // The following line initializes a 2D cols-by-rows array with zeroes
